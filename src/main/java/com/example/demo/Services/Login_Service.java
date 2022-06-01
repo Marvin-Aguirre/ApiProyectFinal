357f9b4545937ef7f0b19e70f5f43f0584a24e5c
Marvin <aguirreestuardo758@gmail.com>
2022-05-31 21:42:20 -0600

    Se modifico el nombre de la package Services

--- src/main/java/com/example/demo/Services/Login_Service.java
+++ src/main/java/com/example/demo/Services/Login_Service.java
@@ -1,4 +1,4 @@
-package com.example.demo.services;
+package com.example.demo.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
