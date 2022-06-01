bf6bc9c09bbce3942f27ffcb2f869e85e60a519f
Marvin <aguirreestuardo758@gmail.com>
2022-05-31 21:40:14 -0600

    Se agrego la clase LoginService

---
+++ src/main/java/com/example/demo/services/Login_Service.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.Login;
+import com.example.demo.Repository.Login_Repository;
+
+@Service
+public class Login_Service {
+	@Autowired
+	private Login_Repository repo;
+  
+  public Login login(String username, String password) {
+	  Login user = repo.findByUsernameAndPassword(username, password);
+  	return user;
+  }
+
+}
+
+	
